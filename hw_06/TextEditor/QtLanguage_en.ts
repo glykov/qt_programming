d1b5c325846e3af9eda876ea18414dcfc0af77c8
Gleb Lykov <glebdom@gmail.com>
2023-04-17 16:52:08 +0300

    Домашняя работа к семинару 6

---
+++ hw_06/TextEditor/QtLanguage_en.ts
@@ -0,0 +1,169 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <location filename="mainwindow.cpp" line="166"/>
+        <source>Text Editor</source>
+        <translation>Текстовый редактор</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="37"/>
+        <location filename="mainwindow.cpp" line="167"/>
+        <source>Open</source>
+        <translation>Открыть</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="50"/>
+        <location filename="mainwindow.cpp" line="168"/>
+        <source>Save</source>
+        <translation>Сохранить</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="63"/>
+        <location filename="mainwindow.cpp" line="169"/>
+        <source>Help</source>
+        <translation>Справка</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="76"/>
+        <location filename="mainwindow.cpp" line="172"/>
+        <source>Read Only</source>
+        <translation>Только для чтения</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="89"/>
+        <location filename="mainwindow.cpp" line="171"/>
+        <source>Change Language</source>
+        <translation>Сменить язык</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="102"/>
+        <location filename="mainwindow.cpp" line="170"/>
+        <source>Change Style</source>
+        <translation>Сменить стиль</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="115"/>
+        <location filename="mainwindow.cpp" line="34"/>
+        <location filename="mainwindow.cpp" line="173"/>
+        <location filename="mainwindow.cpp" line="223"/>
+        <source>Print</source>
+        <translation>Печать</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="20"/>
+        <source>Text file (*.txt)</source>
+        <translation>Текстовый файл (*.txt)</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="26"/>
+        <location filename="mainwindow.cpp" line="174"/>
+        <source>&amp;File</source>
+        <translation>&amp;Файл</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="29"/>
+        <source>&amp;Open</source>
+        <translation>&amp;Открыть</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="28"/>
+        <source>Ctrl+N</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="27"/>
+        <location filename="mainwindow.cpp" line="175"/>
+        <source>&amp;New</source>
+        <translation>&amp;Новый</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="30"/>
+        <source>Ctrl+O</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="31"/>
+        <source>&amp;Save</source>
+        <translation>&amp;Сохранить</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="32"/>
+        <source>Ctrl+S</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="35"/>
+        <source>Ctrl+P</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="37"/>
+        <source>E&amp;xit</source>
+        <translation>В&amp;ыход</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="38"/>
+        <source>Ctrl+Q</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="46"/>
+        <source>&amp;Edit</source>
+        <translation>&amp;Правка</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="47"/>
+        <source>Copy</source>
+        <translation>Копировать</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="48"/>
+        <source>Ctrl+C</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="49"/>
+        <source>Cut</source>
+        <translation>Вырезать</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="50"/>
+        <source>Ctrl+X</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="51"/>
+        <source>Paste</source>
+        <translation>Вставить</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="52"/>
+        <source>Ctrl+V</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="57"/>
+        <source>&amp;View</source>
+        <translation>&amp;Вид</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="58"/>
+        <source>Change &amp;Language</source>
+        <translation>Сменить &amp;язык</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="62"/>
+        <source>&amp;Help</source>
+        <translation>&amp;Помощь</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="63"/>
+        <source>A&amp;bout</source>
+        <translation>О п&amp;рограмме</translation>
+    </message>
+</context>
+</TS>
